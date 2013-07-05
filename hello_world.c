eec7a1947ed0c37e57210779c7b22c69743c4210
Mike Evans <mikeev@gmail.com>
2013-07-05 12:01:33 -0400

    Hello World

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+//=================================================
+// Project: Confucious
+// Language: C 
+// Author: Mike Evans
+// Date: 2013.7.5
+//=================================================
+
+#include <stdio.h>
+main () {
+  int a = 0x982; 
+  printf("Hello, world!\n"); 
+}
